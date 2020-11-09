db31aba4da4924812b9e8bee827c359ee3a51712
rahulpatil22206 <53813613+rahulpatil22206@users.noreply.github.com>
2020-11-09 12:33:57 +0530

    Create power.sh

---
+++ lab4/power.sh
@@ -0,0 +1,14 @@
+echo "Input number"
+read n
+echo "Input power"
+read p
+
+counter=0
+ans=1
+while [ $p -ne $counter ]
+do
+        ans=`expr $ans \* $n`
+        counter=`expr $counter + 1`
+done
+
+echo "$n power of $p is:" $ans
